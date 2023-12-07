6c1ea866ba548b15441b2db5a13c10083e4abcd9
Roberto Alvarado <roberto.alvarado@udla.edu.ec>
2023-12-06 20:32:51 -0500

    Añadio ruta de prueba y primera conexion de get para pruebas

--- arrancar.bat
+++ arrancar.bat
@@ -1,1 +1,3 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin"
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\data\db"
+
+start cmd.exe /k "cd C:\Users\User\Desktop\Curso Nodejs\RedSocialBackend && npm start"
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+//Importar dependencias
+
+const { connection } = require("./database/conecction");
+const express = require("express");
+const cors = require("cors");
+//Mensaje de bienvenida
+
+console.log("Conectado A LA RED SOCIAL");
 //COnexion a la bdd
+connection();
 
 //Crear servidor node
+const app = express();
+const puerto = 3900;
 
 //Configurar cors
+app.use(cors());
 
 //Convertir los datos del body a objetos js
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //cargar conf rutas
+
+//Ruta de prueba
+
+app.get("/ruta-prueba", (req, res) => {
+  return res.status(200).json({
+    id: 1,
+    nombre: Roberto,
+  });
+});
 
 //Poner servidor a escuchar peticiones
 
+app.listen(puerto, () => {
+  console.log("Servidor corriendo en " + puerto);
+});
